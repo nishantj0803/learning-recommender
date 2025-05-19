21ed87e04ac7881fd92ac7f2f2f2e3baef3b96b6
Nishant Jain <157312244+nishantj0803@users.noreply.github.com>
2025-05-19 19:01:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; // Import your main CSS file
 import App from './App';
 // import reportWebVitals from './reportWebVitals'; // <-- Remove this line
-
+import axios from 'axios'; 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
